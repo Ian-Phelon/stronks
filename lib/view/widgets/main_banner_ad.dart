b02fe209ecdeaa05a170724b5b697b1df341e8e8
ian-phelon <ianmacdooglas@gmail.com>
2021-08-08 07:39:53 -0400

    trimmed adHelper

--- lib/view/widgets/main_banner_ad.dart
+++ lib/view/widgets/main_banner_ad.dart
@@ -16,26 +16,6 @@ class AdHelper {
       throw new UnsupportedError('Unsupported platform');
     }
   }
-
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/1033173712";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/4411468910";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
-
-  static String get rewardedAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/5224354917";
-    } else if (Platform.isIOS) {
-      return "ca-app-pub-3940256099942544/1712485313";
-    } else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
 }
 
 class MainBannerAd extends StatefulWidget {
